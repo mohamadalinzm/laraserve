ac3146a97a2d38782fd337ff614e7f77c1edbedf
mohamadalinzm <mohamadalinzm@gmail.com>
2024-12-08 21:37:55 +0330

    Create appointment model

---
+++ src/Models/Appointment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Nzm\LaravelAppointment\Models;
+
+use Appointment\database\factories\AppointmentFactory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Appointment extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'agentable_type',
+        'agentable_id',
+        'clientable_type',
+        'clientable_id',
+        'start_time',
+        'end_time',
+    ];
+
+}
