f367bb1ba90fa1f22ecd0859f34e03417c346651
mohamadalinzm <mohamadalinzm@gmail.com>
2024-12-09 06:03:22 +0330

    Add helper functions for Agent and Client

--- src/Traits/Agentable.php
+++ src/Traits/Agentable.php
@@ -4,6 +4,7 @@ namespace Nzm\Appointment\Traits;
 
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Nzm\Appointment\Models\Appointment;
+use Illuminate\Database\Eloquent\Collection;
 
 trait Agentable
 {
@@ -11,6 +12,30 @@ trait Agentable
     public function agentAppointments(): MorphMany
     {
         return $this->morphMany(Appointment::class, 'agentable');
+    }
+
+    public function getAvailableSlots(): Collection
+    {
+        return $this->agentAppointments()
+            ->whereNull('clientable_id')
+            ->where('start_time', '>', now())
+            ->get();
+    }
+
+    public function getBookedSlots(): Collection
+    {
+        return $this->agentAppointments()
+            ->whereNotNull('clientable_id')
+            ->where('start_time', '>', now())
+            ->get();
+    }
+
+    public function getUpComingBookedSlots(): Collection
+    {
+        return $this->agentAppointments()
+            ->whereNotNull('clientable_id')
+            ->where('start_time', '>', now())
+            ->get();
     }
 
 }
--- src/Traits/Clientable.php
+++ src/Traits/Clientable.php
@@ -2,6 +2,7 @@
 
 namespace Nzm\Appointment\Traits;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Nzm\Appointment\Models\Appointment;
 
@@ -11,6 +12,20 @@ trait Clientable
     public function clientAppointments(): MorphMany
     {
         return $this->morphMany(Appointment::class, 'clientable');
+    }
+
+    public function getBookedSlots(): Collection
+    {
+        return $this->clientAppointments()
+            ->where('start_time', '>', now())
+            ->get();
+    }
+
+    public function getUpComingBookedSlots(): Collection
+    {
+        return $this->clientAppointments()
+            ->where('start_time', '>', now())
+            ->get();
     }
 
 }
